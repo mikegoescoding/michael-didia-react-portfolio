446b89088309235458232b416f851fb7e8059091
michaeldidia <mikegoescoding@gmail.com>
2020-02-18 12:18:58 -0700

    testing multiple classes

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio-container';
+import ContactContainer from './contact';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Michael Di Dia's React Portfolio</h1>
+        <PortfolioContainer />
+        <ContactContainer />
        <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
        </div>
---
+++ src/components/portfolio-container.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+export default class PortfolioContainer extends Component {
+    render() {
+        return (
+            <center>
+            <div>
+                <div>- - - - - - - - - - -</div>
+                <h2>Portfolio items go here...</h2>
+                <h2>Portfolio items go here as well...</h2>
+                <h2>More portfolio items go here...</h2>
+            </div>
+            <div>-</div>
+            <div>
+                <h1>Wait for it...</h1>
+                <h3>...</h3>
+                <h1>Another Portfolio Item !</h1>
+            </div>
+            </center>
+        );
+    }
+}
